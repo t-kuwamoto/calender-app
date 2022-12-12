86788d569b8c7d35b46999eb5bb18e1e4e2bb0da
t-kuwamoto <kuwamoto_takumi@futurescope.jp>
2022-12-12 11:28:38 +0800

    react-environment:edited

--- my-app/routes/web.php
+++ my-app/routes/web.php
@@ -13,6 +13,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Auth::routes();
+
+// Route::get('{any}', function(){
+//     return view('layouts.app');
+// })->where('any','*');
+
+Route::get('/{any}', function () {
+    return view('layouts.app');
+})->where('any','.*');
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Auth::routes();
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
